e9ee2529cc9f4bd510794f9530c75db006caac76
Mateusz Galwas <93769814+Gatiii@users.noreply.github.com>
2023-05-30 10:48:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ dis = pygame.display.set_mode((600, 600))
 
 pygame.display.set_caption("Snake")
 
+black = (0, 0, 0)
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
@@ -23,12 +24,26 @@ y2 = round(random.randrange(0, 580) / 10) * 10
 clock = pygame.time.Clock()
 
 font = pygame.font.SysFont(None, 50)
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+
+def Your_score(score):
+    text = font.render("Your Score: " + str(score), True, black)
+    dis.blit(text, [0, 0])
+
+
+def snake(snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, green, [x[0], x[1], 10, 10])
 
 
 def game_over_message(mess, color):
     msg = font.render(mess, False, color)
     dis.blit(msg, [250, 250])
 
+
+snake_List = []
+Length_of_snake = 1
 
 game = True
 while game:
@@ -54,11 +69,23 @@ while game:
     x1 += x1_change
     y1 += y1_change
     dis.fill(white)
-    pygame.draw.rect(dis, green, [x1, y1, 10, 10])
-    pygame.draw.rect(dis, red, [x2, y2, 10, 10])
 
+    pygame.draw.rect(dis, red, [x2, y2, 10, 10])
+    snake_head = []
+    snake_head.append(x1)
+    snake_head.append(y1)
+    snake_List.append(snake_head)
+    if len(snake_List) > Length_of_snake:
+        del snake_List[0]
+
+    snake(snake_List)
     pygame.display.update()
-    clock.tick(20)
+    if x1 == x2 and y1 == y2:
+        x2 = round(random.randrange(0, 600 - 10) / 10.0) * 10.0
+        y2 = round(random.randrange(0, 600 - 10) / 10.0) * 10.0
+        Length_of_snake += 1
+    Your_score(Length_of_snake - 1)
+    clock.tick(10)
 
 game_over_message("You lost", red)
 pygame.display.update()
